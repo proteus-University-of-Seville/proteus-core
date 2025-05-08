1b4afc7cd140453118b12fcbd73d741bb0479716
Josdelsan <josem_ds@hotmail.com>
2025-05-08 12:44:50 +0200

    Remove unused plugin

--- profiles/basic/plugins/verification/__init__.py
+++
@@ -1,4 +0,0 @@
-from verification.verification import VerificationManager
-
-def register(register_xslt_function, register_qwebchannel_class, register_proteus_component, register_export_strategy):
-    register_qwebchannel_class("verificationManager", VerificationManager)
